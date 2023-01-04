47a5acdf52e30c433633ad9afced64fd6c8ef4ac
jdustindev <jdustindev@gmail.com>
2023-01-04 14:44:35 -0500

    Add placeholder comments for zip code fixing

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -10,5 +10,10 @@ contents = CSV.open(
 contents.each do |row|
     name = row[:first_name]
     zipcode = row[:zipcode]
+
+    # if the zip code is exactly 5 digits, assume that it is ok
+    # if the zip code is more than 5 digits, truncate it to the first 5 digits
+    # if the zip code is less than 5 digits, add zeros to the front until it becomes 5 digits
+
     puts "#{name} #{zipcode}"
 end
