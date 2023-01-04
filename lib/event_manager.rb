b3291744b336d93a03f7eea1333587030655f2b2
jdustindev <jdustindev@gmail.com>
2023-01-04 14:32:15 -0500

    Add basic CSV parsing

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,9 @@
 puts 'Event manager initialized!'
 
-contents = File.read('event_attendees.csv')
-puts contents
+lines = File.readlines('event_attendees.csv')
+lines.each_with_index do |line, index|
+    next if index == 0
+    columns = line.split(',')
+    name = columns[2]
+    puts name
+end
