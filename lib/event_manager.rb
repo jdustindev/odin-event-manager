7ea0bdb017f1b28c5b85e2e1deb2065e8a3d89f2
jdustindev <jdustindev@gmail.com>
2023-01-04 14:50:35 -0500

    Refactor zipcode fixing to its own function

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,18 @@
 require 'csv'
 puts 'Event manager initialized!'
 
+def clean_zipcode(zipcode)
+    if zipcode.nil?
+        '00000'
+    elsif zipcode.length < 5
+        zipcode.rjust(5, '0')
+    elsif zipcode.length > 5
+        zipcode.slice[0..4]
+    else
+        zipcode
+    end
+end
+
 contents = CSV.open(
     'event_attendees.csv',
     headers: true,
