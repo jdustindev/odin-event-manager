d52780a3b677d9b0ee65fbdefa44d267d10770bd
jdustindev <jdustindev@gmail.com>
2023-01-04 14:51:31 -0500

    Finish refactor and use clean_zipcode func

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -21,15 +21,8 @@ contents = CSV.open(
 
 contents.each do |row|
     name = row[:first_name]
-    zipcode = row[:zipcode]
-
-    if zipcode.nil?
-        zipcode = '00000'
-    elsif zipcode.length < 5
-        zipcode = zipcode.rjust(5, '0')
-    elsif zipcode.length > 5
-        zipcode = zipcode.slice[0..4]
-    end
+    
+    zipcode = clean_zipcode(row[:zipcode])
 
     puts "#{name} #{zipcode}"
 end
