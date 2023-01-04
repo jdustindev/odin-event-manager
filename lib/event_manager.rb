769c79e9ee9f0662e2ff960fad465d4c18a00cbb
jdustindev <jdustindev@gmail.com>
2023-01-04 14:48:50 -0500

    Implement zip code fixing

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -11,9 +11,13 @@ contents.each do |row|
     name = row[:first_name]
     zipcode = row[:zipcode]
 
-    # if the zip code is exactly 5 digits, assume that it is ok
-    # if the zip code is more than 5 digits, truncate it to the first 5 digits
-    # if the zip code is less than 5 digits, add zeros to the front until it becomes 5 digits
+    if zipcode.nil?
+        zipcode = '00000'
+    elsif zipcode.length < 5
+        zipcode = zipcode.rjust(5, '0')
+    elsif zipcode.length > 5
+        zipcode = zipcode.slice[0..4]
+    end
 
     puts "#{name} #{zipcode}"
 end
